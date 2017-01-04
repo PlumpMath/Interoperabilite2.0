33f35749ba2331ca4943e5b7656869bad1834b05
Jessy HAVARD <havard.jessy@gmail.com>
2017-01-04 16:44:38 +0100

    feat: wip display choices from json file

--- src/interoperabilite/saisie/InputScanner.java
+++ src/interoperabilite/saisie/InputScanner.java
@@ -2,19 +2,31 @@ package interoperabilite.saisie;
 
 import java.util.*;
 
+import interoperabilite.json.JsonManager;
+
 public class InputScanner {
 
 	public static void main(String[] args) {
+		
+		ArrayList<String> choices = JsonManager.getInstance()
+				.<String>readFromFile("choices.json","./json/", String.class);
+		for (String choice : choices) {
+			if(choice != "{" && choice != "}" && !choice.matches("[-+]?\\d*\\.?\\d+")) {
+				System.out.println(choice);
+			}
+		}
+		System.out.print("\n");
+		
 	    
 		Scanner scanner = null;
 	    try {
 	    	scanner = new Scanner(System.in);
 	       
 	    	// prompt
-		    System.out.print("Enter something: ");
+		    System.out.print("Select an action: ");
 		    String str = scanner.next();
 
-		    System.out.println(String.format("You typed: %s", str));
+		    System.out.println(String.format("Action selected: %s", str));
 	    }
 	    finally {
 	        if(scanner!=null)
